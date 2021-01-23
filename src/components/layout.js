85e171e896ab480d56a2c4f942643569e1b3d7f6
Josh Braun <josh.braun.4@gmail.com>
2021-01-23 11:04:53 -0500

    Added OG tags to site Helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,6 +16,30 @@ export default class Layout extends Component {
         <Helmet>
             <meta charSet="utf-8" />
             <title>{this.props.pageTitle}</title>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, minimal-ui"
+            />
+            <meta property="og:url" content="https://iamjoshbraun.com" />
+            <meta property="og:title" content="Josh Braun" />
+            <meta
+              property="og:image"
+              content="https://iamjoshbraun.netlify.app/files/assets/og-image.jpg"
+            />
+            <meta property="og:description" content="I’m a product designer based in NYC. I’m
+            currently designing machine learning business process automation tools." />
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@iamjoshbraun" />
+            <meta name="twitter:creator" content="@iamjoshbraun" />
+            <meta name="twitter:title" content="Josh Braun" />
+            <meta name="twitter:description" content="I’m a product designer based in NYC. I’m
+            currently designing machine learning business process automation tools." />
+            <meta
+              name="twitter:image"
+              content="https://iamjoshbraun.netlify.app/files/assets/twitter-card.jpg"
+            />
+
         </Helmet>
 
         <Header />
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,9 +8,25 @@ export default class Home extends Component {
 
   render() {
     return (
-      <Layout pageTitle="Contact">
-        <h1>Contact Page</h1>
-        <p>This is your first page</p>
+      <Layout pageTitle="Contact – Josh Braun">
+        <h1>Say Hello 👋🏻</h1>
+        <p>Complete the form below or reach out via email at <a href="mailto:hello@iamjoshbraun.com">hello@iamjoshbraun.com</a>.</p>
+
+        <form name="contact" netlify action="contact#success">
+        <p>
+          <label>Name <input tabindex="1" type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Email <input tabindex="2" type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Note <textarea tabindex="3" name="note"/></label>
+        </p>
+        <p>
+          <button id="send" tabindex="4" type="submit">Send</button>
+        </p>
+      </form>
+      <p id="success_message" className="hidden">Thanks! Your message was sent.</p>
       </Layout>
       )
     }
